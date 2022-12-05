dbe7a4864de1be672432a5605629a7e11a5db452
Petar Bojović <BD103690@branch.otpsrb.loc>
2022-12-05 14:47:42 +0100

    Fixed some typos

--- src/Infra/index.php
+++ src/Infra/index.php
@@ -8,5 +8,5 @@ require_once 'Storage/index.php';
 require_once 'Mailer/index.php';
 require_once 'View/index.php';
 require_once 'Session/index.php';
-require_once 'Mailvalidator/index.php';
+require_once 'MailValidator/index.php';
 require_once 'App/index.php';
